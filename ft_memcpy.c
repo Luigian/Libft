e58f44b6f1c742fea78d926e9b10a430252cd46e
Luis Sanchez <lusanche@e1z3r1p12.42.us.org>
2019-05-29 18:29:32 -0700

    null corrections

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 10:59:31 by lusanche          #+#    #+#             */
-/*   Updated: 2019/05/26 22:34:36 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/05/29 17:29:01 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char *ptr;
 	char *ptr2;
 
+	if (n == 0 || dst == src)
+		return (dst);
 	ptr = (char *)dst;
 	ptr2 = (char *)src;
 	while (n--)
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 13:36:20 by lusanche          #+#    #+#             */
-/*   Updated: 2019/05/26 22:35:41 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/05/29 17:30:02 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char *p_dst;
 	unsigned char *p_src;
 
+	if (len == 0 || dst == src)
+		return (dst);
 	p_dst = (unsigned char *)dst;
 	p_src = (unsigned char *)src;
 	if (p_src < p_dst)
